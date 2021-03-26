7f7356202d9f98200d843e1e549cd4843667cd71
Ayrton M <ayrtonm0@gmail.com>
2021-03-26 16:08:43 +0200

    Login/Logout Function

---
+++ backend/TaskViewAPI/src/test/java/com/example/taskviewAPI/TaskviewAPIMainTests.java
@@ -0,0 +1,13 @@
+package com.example.taskviewAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskviewAPIMainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
